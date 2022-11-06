b82f84aaa1afd11080bf26892416349be4e05b57
Gustavo Viana <gustavorviana@outlook.com>
2022-11-06 13:23:55 -0300

    Update Card.vue

--- interface/src/components/Card.vue
+++ interface/src/components/Card.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+defineProps<{ title?: string }>();
 </script>
 
 <template>
     <div class="card">
-        <div v-if="$slots.header" class="card-header">
-            <slot name="header"></slot>
+        <div v-if="$slots.header || title" class="card-header">
+            <slot name="header">
+                <h5 v-if="title" class="card-title mb-0">{{ title }}</h5>
+            </slot>
         </div>
         <div class="card-body">
             <slot></slot>
